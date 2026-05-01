fd2236ff5e92f7dbcd3e47d620798e8fb2d5d9c0
hiralazure <hiralazure2025@gmail.com>
2026-05-01 18:19:51 +0100

    added global handler middle ware

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ import "dotenv/config";
 import { app } from "./src/app.js";
 import { connectDB } from "./src/common/config/db.js";
 import { configureCors } from "./src/common/config/cors.js";
+import {
+  addTimeStamp,
+  requestLogger,
+} from "./src/common/middleware/customMiddleware.js";
+import { globalErrorHandler } from "./src/common/middleware/errorHandler.js";
 
 const PORT = process.env.PORT || 3000;
 const start = async () => {
-  app.use(express.json())
-  app.use(configureCors())
+  app.use(requestLogger);
+  app.use(addTimeStamp);
+  app.use(express.json());
+  app.use(configureCors());
   await connectDB();
+  app.use(globalErrorHandler);
   app.listen(PORT, (req, res) => {
     console.log(`Server is running at ${PORT}`);
   });
